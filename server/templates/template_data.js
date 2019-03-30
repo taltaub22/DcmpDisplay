6676c6f9aa4bdc31c31874dc70c24cb29293c72c
Tal Taub <taltaub22@gmail.com>
2019-03-30 17:52:06 +0300

    Removed default

--- server/templates/template_data.js
+++ server/templates/template_data.js
@@ -1,11 +1,6 @@
 const matchData = require('../templates_data_imp/match_view')
 
 
-function defaultData(){
-  return {home: 'asds'}
-}
-
 module.exports= {
-  default: defaultData,
   matchData: matchData
 }
